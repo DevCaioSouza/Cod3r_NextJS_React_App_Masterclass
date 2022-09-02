1cbd66ca972b886642e8713fba3bd7f30adfbd11
unknown <caiocbahia@hotmail.com>
2022-09-02 20:16:07 -0300

    primeiro componente gerado

--- fundamentos-do-next/src/pages/index.jsx
+++ fundamentos-do-next/src/pages/index.jsx
@@ -1,4 +1,10 @@
 // criando um componente React baseado em função
 export default function Inicio(){
-  return "Início"
+  return (
+    <div>
+      <h1>Fundamentos de Next.js & React</h1>
+      <h2>Vamos estudar o Framework Next</h2>
+    </div>
+  )
 }
+
